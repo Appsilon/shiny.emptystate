af8b7751c5343838596dc9b9d7fb1038084b0b77
insightsurge <analyticswithharsh@gmail.com>
2023-06-30 13:35:20 +0530

    reactable removed, now using renderTable from shiny

--- tests/testthat/test_app/app.R
+++ tests/testthat/test_app/app.R
@@ -5,7 +5,7 @@ ui <- fluidPage(
   use_empty_state(),
   actionButton("show", "Show empty state!"),
   actionButton("hide", "Hide empty state!"),
-  reactable::reactableOutput("my_table")
+  tableOutput("my_table")
 )
 
 server <- function(input, output, session) {
@@ -24,9 +24,7 @@ server <- function(input, output, session) {
     empty_state_manager$hide()
   })
 
-  output$my_table <- reactable::renderReactable({
-    reactable::reactable(iris)
-  })
+  output$my_table <- renderTable(data.frame(NA))
 }
 
 shinyApp(ui, server)
